936dd75e05a1976320b1dfcd093d053f010ad3ec
Jon Hawkins <jonathan1@houseofhawkins.com>
2019-04-19 19:09:55 +0100

    cleaning up helpers that were not used

--- src/AutoValidator.Tests/Helpers/PrimitiveExtensionsTests.cs
+++ src/AutoValidator.Tests/Helpers/PrimitiveExtensionsTests.cs
@@ -18,40 +18,12 @@ namespace AutoValidator.Tests.Helpers
         class DestinationClass : Interface
         {
             int Interface.Value { get { return 123; } }
-
-            public int PrivateProperty { get; private set; }
-
-            public int PublicProperty { get; set; }
         }
 
         [Test]
         public void Should_find_explicitly_implemented_member()
         {
             PrimitiveHelper.GetFieldOrProperty(typeof(DestinationClass), "Value").Should().NotBeNull();
-        }
-
-        [Test]
-        public void Should_not_flag_only_enumerable_type_as_writeable_collection()
-        {
-            PrimitiveHelper.IsListOrDictionaryType(typeof(string)).Should().BeFalse();
-        }
-
-        [Test]
-        public void Should_flag_list_as_writable_collection()
-        {
-            PrimitiveHelper.IsListOrDictionaryType(typeof(int[])).Should().BeTrue();
-        }
-
-        [Test]
-        public void Should_flag_generic_list_as_writeable_collection()
-        {
-            PrimitiveHelper.IsListOrDictionaryType(typeof(List<int>)).Should().BeTrue();
-        }
-
-        [Test]
-        public void Should_flag_dictionary_as_writeable_collection()
-        {
-            PrimitiveHelper.IsListOrDictionaryType(typeof(Dictionary<string, int>)).Should().BeTrue();
-        }
+        }        
     }
 }
